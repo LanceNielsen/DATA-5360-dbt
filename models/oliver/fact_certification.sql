a15fc0af5ee77b887be0bb5d75b82aaea87eade4
Lance Nielsen <a02259757@usu.edu>
2025-10-25 14:06:57 +0000

    Added a new source and fact table

---
+++ models/oliver/fact_certification.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized = 'table',
+    schema = 'dw_oliver'
+) }}
+
+select
+    d.date_key,
+    e.employee_key,
+    ec.certification_name,
+    ec.certification_cost
+from {{ref('stg_employee_certifications')}} ec
+inner join {{ref('dim_date')}} d
+    on d.date_key = ec.certification_awarded_date
+inner join {{ref('oliver_dim_employee')}} e
+    on e.employee_key = ec.employee_id
+
