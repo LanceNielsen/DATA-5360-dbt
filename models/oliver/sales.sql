c851dfe28137437bbdaf1d3fc209a3ab8812d592
Lance Nielsen <a02259757@usu.edu>
2025-11-08 03:30:30 +0000

    test fixed

--- models/oliver/sales.sql
+++ models/oliver/sales.sql
@@ -4,15 +4,15 @@
 )}}
 
 select
-    c.First_Name as "Customer First Name",
-    c.Last_Name as "Customer Last Name",
-    d.date_day as "Sale Date",
-    s.Store_name as "Store Name",
-    p.product_name as "Product Name",
-    f.quantity as "quantity",
-    f.dollars_sold as "Dollars Sold"
+    c.First_Name as CustomerFirstName,
+    c.Last_Name as CustomerLastName,
+    d.date_day as SaleDate,
+    s.Store_name as StoreName,
+    p.product_name as ProductName,
+    f.quantity as quantity,
+    f.dollars_sold as DollarsSold
 from {{ ref('fact_sales')}} f
-    LEFT JOIN {{ ref('dim_customer') }} c on f.cust_key = c.cust_key
+    LEFT JOIN {{ ref('oliver_dim_customer') }} c on f.cust_key = c.cust_key
     LEFT JOIN {{ ref('oliver_dim_date') }} d on f.date_key = d.date_key
     LEFT JOIN {{ ref('oliver_dim_store') }} s on f.store_key = s.store_key
     LEFT JOIN {{ ref('oliver_dim_product') }} p on f.product_key = p.product_key
